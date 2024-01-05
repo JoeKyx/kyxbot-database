62007c085e56c6e1b0d7d0c617880a97066a3a29
Joe <joe64gg@gmail.com>
2024-01-05 21:40:29 +0100

    delete js files

--- dist/migrate.js
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { db, client } from "./";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-// This will run migrations on the database, skipping the ones already applied
-await migrate(db, { migrationsFolder: "./drizzle" });
-// Don't forget to close the connection, otherwise the script will hang
-await client.end();
